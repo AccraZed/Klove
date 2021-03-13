2ca5929b442fedc9648a395fa83997aebdf3d61f
root <root@DESKTOP-AVAO68C.localdomain>
2021-03-13 12:29:36 -0500

    initial commit for front end

---
+++ src/frontend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/prefrences", function (req, res) {
+    res.render("prefrences");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "")
+  port = 8080;
+
+app.listen(port, function() {
+  console.log("Server started on port " + port + "...");
+});
