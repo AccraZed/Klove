28dfa305e02ae7b48e6a9f651ac59c4679bd355c
Samuelmm97 <Sammymenaged@yahoo.com>
2021-03-14 13:24:28 -0400

    added multiple forms about address

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const { property } = require("lodash");
+//const { property } = require("lodash");
 const { spawn } = require("child_process");
 
 const app = express();
@@ -19,24 +19,7 @@ app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  res.render("home");
-});
-
-// search button 
-app.post("/search", function (req, res) {
-  const search = req.body.search;
-  res.redirect("/report");
-});
-
-app.get("/report", function (req, res) {
-  const python = spawn('python', ['src/main.py']);
-  //console.log(python);
-  python.stdout.on('data', function (data) {
-    console.log("test " + data.toString());
-  });
-
-  var property = {
+var property = {
     address: "1234 example st",
     price: "$100 000 000",
     school: "A-",
@@ -44,7 +27,36 @@ app.get("/report", function (req, res) {
     walk_score: 90,
     bike_score: 94,
     transit_score: 80,
-  }
+}
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+
+
+// search button 
+app.post("/search", function (req, res) {
+
+  const houseNumber = req.body.houseNumber;
+  const streetName = req.body.streetName;
+  const city = req.body.city;
+  const state = req.body.state;
+  const zip = req.body.zip;
+  property.address = houseNumber + " " + streetName;
+
+  const python = spawn('python', ['src/main.py', houseNumber, streetName, city, state, zip]);
+  //console.log(python);
+  python.stdout.on('data', function (data) {
+    console.log(data);
+  });
+
+  res.redirect("/report");
+});
+
+app.get("/report", function (req, res) {
+
+  console.log(property.address);
   res.render("report", { property: property});
 });
 
