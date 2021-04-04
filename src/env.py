9f0a021b41db6103da5cfb38ecdd46d243b2578a
accrazed <accrazed@gmail.com>
2021-04-04 16:07:10 -0400

    shrug

--- src/env.py
+++ src/env.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 KEY_GOOGLE_GEO = 'AIzaSyBfC5zmHQfJwlkoecC0OuwTF_WE8UdDBXE'
-=======
-KEY_GOOGLE_GEO = 'AIzaSyAa7tDAtxmrVCklE-l47I0jZ3t7hh7tBEw'
->>>>>>> a1867a4... Added videos
 KEY_WALK_SCORE='8226a2c85d45ac6d13abd490f22f5f69'
